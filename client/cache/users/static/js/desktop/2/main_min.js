652ded543231b19ef2b911f7e47d844b7c330bdb
cloudgnome <ivan.mail.digital@gmail.com>
2025-07-06 18:58:47 +0200

    index template builder

--- client/cache/users/static/js/desktop/2/main_min.js
+++
@@ -1,3 +0,0 @@
-export class Menu{constructor(){this.bindEvents();this.activeTab=Dom.query(".tab.active");this.activeButton=Dom.query(".tabButton.active");}
-bindEvents(){Dom.query("#tab-buttons button").each((button)=>{button.on("click",this.activateTab.bind(this));});}
-activateTab(e){const btn=e.target.closest("button");if(!btn)return;const tabKey=btn.get('data-tab');if(!tabKey)return;const newTab=Dom.query(`.tab[data-tab=${tabKey}]`);if(!newTab)return;this.activeTab.removeClass("active");this.activeButton.removeClass("active");newTab.addClass("active");btn.addClass("active");this.activeTab=newTab;this.activeButton=btn;}}
--- server/anwendung/context.py
+++ server/anwendung/context.py
@@ -2,6 +2,6 @@ from anwendung.services.minify.minify import MinifyService
 
 def index_context(request):
     return {
-        "css_version": MinifyService.get_version("css"),
-        "js_version": MinifyService.get_version("js")
+        "css_version": MinifyService.get_version("css", 'desktop'),
+        "js_version": MinifyService.get_version("js", 'desktop')
     }
